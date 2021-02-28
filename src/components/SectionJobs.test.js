5e42bffec58b85fe1c3b1f9457c38eb9630cef13
Thiago <tbrm.bit@gmail.com>
2021-02-28 18:39:44 -0300

    Removed test from component

--- src/components/SectionJobs.test.js
+++ src/components/SectionJobs.test.js
@@ -5,8 +5,3 @@ test('The component shoud be exist', () => {
   const component = render(SectionJobs);
   expect(component).toBeTruthy();
 });
-
-test('makes a GET request to jobs', () => {
-  let mockAPI = fireEvent.call(window, "fetch");
-  waitFor(() => expect(mockAPI).toHaveBeenCalledWith("http://www.mocky.io/v2/5d6fb6b1310000f89166087b", { method: "GET" }));
-});
